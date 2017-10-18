272c1441c1b682d679e3f63c11bf758d5efafbfe
donese <donese@DONESE1.asiapacific.hpqcorp.net>
2017-10-19 04:15:55 +0800

    Dynamic endpoint for prime service

--- primestore/src/main/java/com/dxc/prime/PrimeStoreController.java
+++ primestore/src/main/java/com/dxc/prime/PrimeStoreController.java
@@ -1,6 +1,7 @@
 package com.dxc.prime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,13 +15,17 @@ public class PrimeStoreController {
 	@Autowired
 	private PrimeStore store;
 
+	@Value("${endpoint.prime}")
+	private String primeEndpoint; 
+	
 	@RequestMapping(value = "/{nthPrime}", method = RequestMethod.GET)
 	public PrimeResponse findPrime(@PathVariable("nthPrime") int nthPrime) {
 		StoredPrime prime = store.findOne(nthPrime);
 		if (prime == null) {
 			RestTemplate client = new RestTemplate();
-			PrimeResponse primeResult = client.getForObject("http://localhost:8080/compute/" + nthPrime,
+			PrimeResponse primeResult = client.getForObject(primeEndpoint + nthPrime,
 					PrimeResponse.class);
+//			System.out.println("primeEndpoint: " + primeEndpoint);
 //			PrimeResponse primeResult = new PrimeResponse(-1L);
 			prime = new StoredPrime(nthPrime, primeResult.getPrimeNumber());	
 			store.save(prime);
